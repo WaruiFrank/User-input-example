43b496af8418e763555ef83d3eb00dfe8e0ae003
WaruiFrank <francisgithinji03@gmail.com>
2024-01-11 10:45:24 +0300

    user input

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php?>
+    <form action="index.php" method="get">
+     Name: <input type="text" name="name">
+</form>
+
+</body>
+</html>
